f5e764d01af7795a39512f3f31148b369909c29f
maryammozafary <maryam.mozafary.76hm@gmail.com>
2023-05-03 09:45:53 +0330

    remove register/signup feature

--- users/views.py
+++ users/views.py
@@ -44,30 +44,6 @@ def logoutUser(request):
     return redirect('login')
 
 
-def registerUser(request):
-    page = 'register'
-    form = CustomUserCreationForm()
-
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.username = user.username.lower()
-            user.save()
-
-            messages.success(request, 'User account was created!')
-
-            login(request, user)
-            return redirect('edit-account')
-
-        else:
-            messages.success(
-                request, 'An error has occurred during registration')
-
-    context = {'page': page, 'form': form}
-    return render(request, 'users/login_register.html', context)
-
-
 def profiles(request):
     profiles, search_query = searchProfiles(request)
 
