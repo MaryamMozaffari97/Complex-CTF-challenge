2322e5d05b6eba09960c0f02a81f4c834685c351
maryammozafary <maryam.mozafary.76hm@gmail.com>
2023-05-09 08:10:02 +0330

    apply black formatter

--- projects/utils.py
+++ projects/utils.py
@@ -2,10 +2,11 @@ from .models import Project, Tag
 from django.db.models import Q
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
+from xml.sax.handler import ContentHandler, EntityResolver
+
 
 def paginateProjects(request, projects, results):
-
-    page = request.GET.get('page')
+    page = request.GET.get("page")
     paginator = Paginator(projects, results)
 
     try:
@@ -17,12 +18,12 @@ def paginateProjects(request, projects, results):
         page = paginator.num_pages
         projects = paginator.page(page)
 
-    leftIndex = (int(page) - 4)
+    leftIndex = int(page) - 4
 
     if leftIndex < 1:
         leftIndex = 1
 
-    rightIndex = (int(page) + 5)
+    rightIndex = int(page) + 5
 
     if rightIndex > paginator.num_pages:
         rightIndex = paginator.num_pages + 1
@@ -33,18 +34,17 @@ def paginateProjects(request, projects, results):
 
 
 def searchProjects(request):
+    search_query = ""
 
-    search_query = ''
-
-    if request.GET.get('search_query'):
-        search_query = request.GET.get('search_query')
+    if request.GET.get("search_query"):
+        search_query = request.GET.get("search_query")
 
     tags = Tag.objects.filter(name__icontains=search_query)
 
     projects = Project.objects.distinct().filter(
-        Q(title__icontains=search_query) |
-        Q(description__icontains=search_query) |
-        Q(owner__name__icontains=search_query) |
-        Q(tags__in=tags)
+        Q(title__icontains=search_query)
+        | Q(description__icontains=search_query)
+        | Q(owner__name__icontains=search_query)
+        | Q(tags__in=tags)
     )
     return projects, search_query
