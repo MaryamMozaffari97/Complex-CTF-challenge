b805c7069f7085173b9cae402c3605105f7eced9
maryammozafary <maryam.mozafary.76hm@gmail.com>
2023-05-10 17:50:46 +0330

    remove create project functionality

--- projects/urls.py
+++ projects/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path("", views.projects, name="projects"),
     path("project/<str:pk>/", views.project, name="project"),
-    path("create-project/", views.createProject, name="create-project"),
 ]
