92e5b15d8f033b5eec944353aa7df385b57f40b0
maryammozafary <maryam.mozafary.76hm@gmail.com>
2023-05-10 18:20:17 +0330

    apply black formatter

--- projects/migrations/0005_auto_20210622_0911.py
+++ projects/migrations/0005_auto_20210622_0911.py
@@ -5,24 +5,27 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('users', '0003_auto_20210617_1510'),
-        ('projects', '0004_project_owner'),
+        ("users", "0003_auto_20210617_1510"),
+        ("projects", "0004_project_owner"),
     ]
 
     operations = [
         migrations.AlterModelOptions(
-            name='project',
-            options={'ordering': ['created']},
+            name="project",
+            options={"ordering": ["created"]},
         ),
         migrations.AddField(
-            model_name='review',
-            name='owner',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='users.profile'),
+            model_name="review",
+            name="owner",
+            field=models.ForeignKey(
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                to="users.profile",
+            ),
         ),
         migrations.AlterUniqueTogether(
-            name='review',
-            unique_together={('owner', 'project')},
+            name="review",
+            unique_together={("owner", "project")},
         ),
     ]
--- projects/models.py
+++ projects/models.py
@@ -3,48 +3,48 @@ import uuid
 
 from django.db.models.deletion import CASCADE
 from users.models import Profile
+
 # Create your models here.
 
 
 class Project(models.Model):
-    owner = models.ForeignKey(
-        Profile, null=True, blank=True, on_delete=models.CASCADE)
+    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    featured_image = models.ImageField(
-        null=True, blank=True, default="default.jpg")
+    featured_image = models.ImageField(null=True, blank=True, default="default.jpg")
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
-    tags = models.ManyToManyField('Tag', blank=True)
+    tags = models.ManyToManyField("Tag", blank=True)
     vote_total = models.IntegerField(default=0, null=True, blank=True)
     vote_ratio = models.IntegerField(default=0, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True,
-                          primary_key=True, editable=False)
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False
+    )
 
     def __str__(self):
         return self.title
 
     class Meta:
-        ordering = ['-vote_ratio', '-vote_total', 'title']
+        ordering = ["-vote_ratio", "-vote_total", "title"]
 
     @property
     def imageURL(self):
         try:
             url = self.featured_image.url
         except:
-            url = ''
+            url = ""
         return url
 
     @property
     def reviewers(self):
-        queryset = self.review_set.all().values_list('owner__id', flat=True)
+        queryset = self.review_set.all().values_list("owner__id", flat=True)
         return queryset
 
     @property
     def getVoteCount(self):
         reviews = self.review_set.all()
-        upVotes = reviews.filter(value='up').count()
+        upVotes = reviews.filter(value="up").count()
         totalVotes = reviews.count()
 
         ratio = (upVotes / totalVotes) * 100
@@ -56,19 +56,20 @@ class Project(models.Model):
 
 class Review(models.Model):
     VOTE_TYPE = (
-        ('up', 'Up Vote'),
-        ('down', 'Down Vote'),
+        ("up", "Up Vote"),
+        ("down", "Down Vote"),
     )
     owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     body = models.TextField(null=True, blank=True)
     value = models.CharField(max_length=200, choices=VOTE_TYPE)
     created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True,
-                          primary_key=True, editable=False)
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False
+    )
 
     class Meta:
-        unique_together = [['owner', 'project']]
+        unique_together = [["owner", "project"]]
 
     def __str__(self):
         return self.value
@@ -77,8 +78,9 @@ class Review(models.Model):
 class Tag(models.Model):
     name = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True,
-                          primary_key=True, editable=False)
+    id = models.UUIDField(
+        default=uuid.uuid4, unique=True, primary_key=True, editable=False
+    )
 
     def __str__(self):
         return self.name
