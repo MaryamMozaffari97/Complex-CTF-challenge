fcd6083d9ac0737689f3b8f0b3cf9d5f44da11c5
maryammozafary <maryam.mozafary.76hm@gmail.com>
2023-05-19 21:08:15 +0330

    improve single project view

--- projects/urls.py
+++ projects/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.ProjectListView.as_view(), name="projects"),
-    path("<str:pk>/", views.project, name="project"),
+    path("<str:pk>/", views.ProjectDetailView.as_view(), name="project"),
 ]
