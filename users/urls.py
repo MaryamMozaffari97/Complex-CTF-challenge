029ad7f0ee506c131539522eec2a3832b64e68f6
maryammozafary <maryam.mozafary.76hm@gmail.com>
2023-05-19 15:56:21 +0330

    update url endpoints to be more descriptive

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path("login/", views.loginUser, name="login"),
     path("reset_password/", views.resetPassword, name="reset_password"),
     path("", views.profiles, name="profiles"),
-    path("profile/<str:pk>/", views.userProfile, name="user-profile"),
+    path("profiles/<str:pk>/", views.userProfile, name="user-profile"),
     path("create-message/<str:pk>/", views.createMessage, name="create-message"),
     path("feedback/", views.createFeedback, name="feedback"),
 ]
