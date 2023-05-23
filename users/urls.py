404d6db3dfba64591b2dfcc8cb42b7bf52dd9f47
maedemhm <maede.mhmdiva99@gmail.com>
2023-05-23 09:55:43 +0330

    apply black formatter on the remaining files

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,10 @@ urlpatterns = [
     path("reset_password/", views.ResetPasswordView.as_view(), name="reset-password"),
     path("", views.ProfileListView.as_view(), name="profiles"),
     path("profiles/<str:pk>/", views.ProfileDetailView.as_view(), name="user-profile"),
-    path("create-message/<str:pk>/", views.createMessage, name="create-message"),
-    path("feedback/", views.createFeedback, name="feedback"),
+    path(
+        "create-message/<str:pk>/",
+        views.createMessage,
+        name="create-message",
+    ),
+    path("feedback/", views.createFeedback, name="create-feedback"),
 ]
