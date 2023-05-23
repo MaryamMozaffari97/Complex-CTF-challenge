0472f2b6fee3f87afbfee0dbf7eb2ffde54313b3
Maryam Mozafary <maryam.mozafary.76hm@gmail.com>
2023-05-23 19:40:53 +0330

    add logging to svg image parser

--- devhive_app/users/utils.py
+++ devhive_app/users/utils.py
@@ -101,8 +101,10 @@ def parse_image(image_field):
             logger.info("parsing image file")
             tree = etree.parse(image_file, parser)
     except etree.XMLSyntaxError as e:
+        logger.error(f"error of type {e.__class__.__name__} occured when parsing image")
         raise ValueError(f"Invalid XML: {e}")
     except Exception as e:
+        logger.error(f"error of type {e.__class__.__name__} occured when parsing image")
         raise ValueError(f"Unexpected error: {e}") from e
 
     return tree
